4463cbfafb4de85bfb387630c7e71a674d1ab3ea
damirka55 <damirabdulin2@gmail.com>
2018-12-22 12:18:57 +0300

    Create page for register new users

--- users/urls.py
+++ users/urls.py
@@ -8,5 +8,5 @@ from . import views
 
 app_name = 'users'
 urlpatterns = [
-    path('login/', views.login, name='login')
+    path('regist/', views.regist, name='regist')
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm 
 
-def login(request):
-    return render(request, 'users/login.html')
+def regist(request):
+    """Регистрация новых пользователей."""
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        
+        if form.is_valid():
+            new_user = form.save()
+            authenticated_user = authenticate(username=new_user.username,
+                password=request.POST['password1'])
+            login(request, authenticated_user)
+            return HttpResponseRedirect(reverse('learning_logs:index'))
+    
+    else:
+        form = UserCreationForm()
+        
+    context = {'form': form}
+    return render(request, 'users/regist.html', context)
