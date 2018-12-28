294edf03bade16fdcc028a8417c1abf9c127140f
damirka55 <damirabdulin2@gmail.com>
2018-12-28 19:31:51 +0300

    Add login_required for all views in learning_logs, except index.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -17,7 +17,8 @@ def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
-    
+
+@login_required
 def topic(request, topic_id):
     """Выводит одну тему и все ее записи."""
     topic = Topic.objects.get(id=topic_id)
@@ -25,6 +26,7 @@ def topic(request, topic_id):
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
+@login_required
 def new_topic(request):
     """Определяет новую тему."""
     if request.method != 'POST':
@@ -40,6 +42,7 @@ def new_topic(request):
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
     
+@login_required
 def new_entry(request, topic_id):
     """Добавляет новую запись по конкретной теме."""
     topic = Topic.objects.get(id=topic_id)
@@ -60,6 +63,7 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
     """Редактирует существующую запись."""
     entry = Entry.objects.get(id=entry_id)
