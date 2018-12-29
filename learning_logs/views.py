1714ce187d6f1fd98932745d588c33898430c91c
damirka55 <damirabdulin2@gmail.com>
2018-12-29 16:12:11 +0300

    Protecting a User’s Topics

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -71,6 +71,8 @@ def edit_entry(request, entry_id):
     """Редактирует существующую запись."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
     
     if request.method != 'POST':
         #Исходный запрос; форма заполняется данными текущей записи.
