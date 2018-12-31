ea543dccc67d01cc03843c7d2d89af1a3448bde7
damirka55 <damirabdulin2@gmail.com>
2018-12-31 12:56:46 +0300

    Modifying new_topic.html.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'ru-ru'
+LANGUAGE_CODE = 'en-en'
 
 TIME_ZONE = 'UTC'
 
